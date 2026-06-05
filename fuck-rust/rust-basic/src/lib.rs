e71491df1f14d928666fb312417e765c187a10a2
xyzBits <lidf077@gmail.com>
2026-06-05 08:18:13 +0800

    update

--- fuck-rust/rust-basic/src/lib.rs
+++ fuck-rust/rust-basic/src/lib.rs
@@ -1,17 +1,7 @@
+#[allow(clippy::all)]
 mod lifetime;
+
+#[allow(clippy::all)]
 mod learn_async;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
